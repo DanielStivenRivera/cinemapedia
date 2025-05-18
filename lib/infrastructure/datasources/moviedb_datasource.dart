037d2978dba08073726e8b2ae372e5bf94badada
DanielStivenRivera <danestiv0610@gmail.com>
2025-05-18 16:25:25 -0500

    implementation of clean architecture

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environments.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_db_response.dart';
 import 'package:dio/dio.dart';
 
 class MovieDBDataSource extends MoviesDataSource {
@@ -17,7 +19,12 @@ class MovieDBDataSource extends MoviesDataSource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies =
+        movieDbResponse.results
+            .where((e) => e.posterPath != 'no-poster')
+            .map((e) => MovieMapper.movieDbToEntity(e))
+            .toList();
     return movies;
   }
 }
