0cd73f524cc0295a170cea46527bd22f2bbe1ebf
DanielStivenRivera <danestiv0610@gmail.com>
2025-06-04 04:57:56 -0500

    stateful shell route done

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  const CustomBottomNavigation({required this.navigationShell, super.key});
+
+  final StatefulNavigationShell navigationShell;
+
+  void onItemTap(BuildContext context, int index) {
+    navigationShell.goBranch(index, initialLocation: false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      onTap: (value) => onItemTap(context, value),
+      currentIndex: navigationShell.currentIndex,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
